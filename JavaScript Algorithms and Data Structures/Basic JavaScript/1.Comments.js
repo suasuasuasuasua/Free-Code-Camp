0d57d8bab5c2525ebd75998abebf0c7f66aa1a05
Justin Hoang <justinhoang124@hotmail.com>
2021-05-25 11:24:48 -0600

    started

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/1.Comments.js
@@ -0,0 +1,12 @@
+/*
+  This is the first assignment in the JavaScript series.
+
+  In this assignment, we learn how to comment.
+
+  You can just think of it like C++ comments
+*/
+// this is an in-line comment
+
+/*
+this is a multiline comment
+*/
